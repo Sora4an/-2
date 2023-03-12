c0e88a45a2086503f383623839d3d74468cf942f
Sora4an <91047228+Sora4an@users.noreply.github.com>
2023-03-12 14:03:01 +0300

    Add files via upload

---
+++ A. Разные/A. Разные/A. Разные.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+#include <unordered_set>
+
+int main() {
+    size_t n;
+    std::cin >> n;
+    std::unordered_set<int> s;
+    for (size_t i = 0; i < n; ++i) {
+        int a;
+        std::cin >> a;
+        s.insert(a);
+    }
+    std::cout << s.size();
+    return 0;
+}
