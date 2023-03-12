a7fa16852b48f33ae6a60439243b1b96f98b916a
Sora4an <91047228+Sora4an@users.noreply.github.com>
2023-03-12 14:17:04 +0300

    Add files via upload

---
+++ D. Правильная скобочная последовательность/D. Правильная скобочная последовательность/D. Правильная скобочная последовательность.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <stack>
+
+bool RightBracketSequence(std::string seq) {
+    std::stack<char> st;
+    for (auto c : seq) {
+        if (c == '(' || c == '[' || c == '{') { 
+            st.push(c);
+        }
+        else if (!st.empty()) {
+            if (c == ')' && st.top() == '(') {
+                st.pop();
+            }
+            else if (c == ']' && st.top() == '[') {
+                st.pop();
+            }
+            else if (c == '}' && st.top() == '{') {
+                st.pop();
+            }
+            else {
+                return false;
+            }
+        }
+        else {
+            return false;
+        }
+    }
+    if (!st.empty()) {
+        return false;
+    }
+    return true;
+}
+
+int main() {
+    std::string seq;
+    std::cin >> seq;
+    std::cout << (RightBracketSequence(seq) ? "yes" : "no");
+}
